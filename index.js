34d5b2efcc2f93a7385dbf6c0552b58827792bda
Chris Troutner <chris.troutner@gmail.com>
2021-03-25 19:37:05 -0700

    feat(Access Controller): Added pay-to-write access controller

--- index.js
+++ index.js
@@ -7,20 +7,67 @@
 
 'use strict'
 
-const BCHJS = require('@psf/bch-js')
+// const Util = require('./lib/util')
+// const util = new Util()
 
-const Util = require('./lib/util')
-const util = new Util()
+const OrbitDB = require('orbit-db')
 
-let _this // local global for 'this'.
+// Define the pay-to-write access controller.
+const AccessControllers = require('orbit-db-access-controllers')
+const PayToWriteAccessController = require('./pay-to-write-access-controller')
+AccessControllers.addAccessController({
+  AccessController: PayToWriteAccessController
+})
 
-class BoilplateLib {
-  constructor () {
-    _this = this
+// let _this // local global for 'this'.
 
-    _this.bchjs = new BCHJS()
-    _this.util = util
+class PayToWriteDB {
+  constructor (config) {
+    // _this = this
+
+    // Input Validation
+    if (!config.ipfs) {
+      throw new Error(
+        'Must past an instance of ipfs when instantiationg the PayToWriteDB class.'
+      )
+    } else {
+      this.ipfs = config.ipfs
+    }
+
+    // _this.util = util
+  }
+
+  // Returns the handle to a key-value store OrbitDB with pay-to-write
+  // access control. The key is a BCH TXID that burned tokens, to pay for the
+  // write.
+  async createDb () {
+    try {
+      const orbitdb = await OrbitDB.createInstance(this.ipfs, {
+        // directory: "./orbitdb/examples/eventlog",
+        directory: './orbitdb/dbs/keyvalue',
+        AccessControllers: AccessControllers
+      })
+
+      const options = {
+        accessController: {
+          type: 'othertype',
+          write: ['*']
+        }
+      }
+
+      // Create the key-value store.
+      const DB_NAME = 'keyvalue003'
+      this.db = await orbitdb.keyvalue(DB_NAME, options)
+
+      // Load data persisted to the hard drive.
+      await this.db.load()
+
+      return this.db
+    } catch (err) {
+      console.error('Error in createDb()')
+      throw err
+    }
   }
 }
 
-module.exports = BoilplateLib
+module.exports = PayToWriteDB
